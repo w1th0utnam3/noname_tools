eba93e8e7d8eaedb6478f212694500c3cd714833
Fabian Löschner <w1th0utnam3@users.noreply.github.com>
2016-05-29 23:15:01 +0200

    Added new algorithms
    Added n_subranges to split range into equal sized subranges. Added
    find_unequal_successor to find first element that does not match it's
    predecessor. Added strict_unique_copy overload.

--- src/noname_tools/algorithm_tools.h
+++ src/noname_tools/algorithm_tools.h
@@ -1,20 +1,78 @@
 #pragma once
 
+#include <vector>
 #include <iterator>
 
-namespace noname {
+namespace noname
+{
+	namespace tools
+	{
+		// TODO: Implement method to split range into unique and non-unique elements
 
-	namespace tools {
+		//! Divides a range in n (nearly) equal sized subranges and writes every subrange's begin and end iterator into dest without duplicates (i.e. dest will have n+1 entries)
+		template <typename InputIt, typename OutputIt>
+		void n_subranges(InputIt first, InputIt last, OutputIt dest, std::size_t n)
+		{
+			// Inspired from: https://codereview.stackexchange.com/questions/106773/dividing-a-range-into-n-sub-ranges
 
-		// TODO: Add strict_unique_copy implementation without p
-		// TODO: Add some kind of method to split range into unique and non-unique elements
+			static_assert(std::is_same<InputIt, typename std::iterator_traits<OutputIt>::value_type>::value,
+				"Error: The output iterator value type has to be the same as the input iterator type!");
+
+			if (n == 0) return;
+
+			const auto dist = std::distance(first, last);
+			n = std::min<size_t>(n, dist);
+			const auto chunk = dist / n;
+			const auto remainder = dist % n;
+
+			*dest++ = first;
+
+			for (size_t i = 0; i < n - 1; i++) {
+				first = std::next(first, chunk + (remainder ? 1 : 0));
+				*dest++ = first;
+
+				if (remainder) remainder -= 1;
+			}
+
+			*dest++ = last;
+		}
+
+		//! Returns the first element in the specified range that is unequal to its predecessor
+		template <typename InputIt>
+		InputIt find_unequal_successor(InputIt first, InputIt last)
+		{
+			if (first != last) {
+				InputIt next = std::next(first);
+				while (next != last) {
+					if (*first != *next) return next;
+					first = next;
+					++next;
+				}
+			}
+			return last;
+		}
+
+		//! Returns the first element in the specified range that is unequal to its predecessor, uses p to compare two elements for equality
+		template <typename InputIt, typename BinaryPredicate>
+		InputIt find_unequal_successor(InputIt first, InputIt last, BinaryPredicate p)
+		{
+			if (first != last) {
+				InputIt next = std::next(first);
+				while (next != last) {
+					if (!p(*first, *next)) return next;
+					first = next;
+					++next;
+				}
+			}
+			return last;
+		}
 
 		//! Copies the elements from the specified range to dest in such a way that all groups of consecutive equal objects are omitted
-		template< class InputIt, class OutputIt, class BinaryPredicate >
-		OutputIt strict_unique_copy(InputIt first, InputIt last, OutputIt dest, BinaryPredicate p)
+		template <typename InputIt, typename OutputIt>
+		OutputIt strict_unique_copy(InputIt first, InputIt last, OutputIt dest)
 		{
 			// Return if input range is empty
-			if(first == last) return dest;
+			if (first == last) return dest;
 
 			// Variable for result of previous comparison
 			bool prev_check = false;
@@ -23,11 +81,11 @@ namespace noname {
 			// Next element of the range
 			auto next = std::next(first);
 
-			while(next != last) {
+			while (next != last) {
 				// Compare current and next element
-				cur_check = p(*first,*next);
-				// Copy if presceding and next elment are not equal to current elemnt
-				if(!prev_check && !cur_check) {
+				cur_check = (*first == *next);
+				// Copy if element does not belong to a group
+				if (!prev_check && !cur_check) {
 					*dest++ = *first;
 				}
 
@@ -37,11 +95,42 @@ namespace noname {
 			}
 
 			// Copy last element
-			if(!prev_check) *dest++ = *first;
+			if (!prev_check) *dest++ = *first;
 
 			return dest;
 		}
 
-	}
+		//! Copies the elements from the specified range to dest in such a way that all groups of consecutive equal objects are omitted, uses p to compare elements for equality
+		template <typename InputIt, typename OutputIt, typename BinaryPredicate>
+		OutputIt strict_unique_copy(InputIt first, InputIt last, OutputIt dest, BinaryPredicate p)
+		{
+			// Return if input range is empty
+			if (first == last) return dest;
 
+			// Variable for result of previous comparison
+			bool prev_check = false;
+			// Current comparison result
+			bool cur_check = false;
+			// Next element of the range
+			auto next = std::next(first);
+
+			while (next != last) {
+				// Compare current and next element
+				cur_check = p(*first, *next);
+				// Copy if element does not belong to a group
+				if (!prev_check && !cur_check) {
+					*dest++ = *first;
+				}
+
+				prev_check = cur_check;
+				first = next;
+				next = std::next(first);
+			}
+
+			// Copy last element
+			if (!prev_check) *dest++ = *first;
+
+			return dest;
+		}
+	}
 }
