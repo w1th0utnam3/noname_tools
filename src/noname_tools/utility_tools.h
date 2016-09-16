59f64e06de128456f831e17cebe1f40ae7430229
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2016-09-16 12:18:47 +0200

    Fixed wrong character

--- src/noname_tools/utility_tools.h
+++ src/noname_tools/utility_tools.h
@@ -1,6 +1,6 @@
 //	MIT License
 //
-//	Copyright (c) 2016 Fabian Löschner
+//	Copyright (c) 2016 Fabian LÃ¶schner
 //
 //	Permission is hereby granted, free of charge, to any person obtaining a copy
 //	of this software and associated documentation files (the "Software"), to deal
