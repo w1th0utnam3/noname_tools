99d4cba33b8b091bf0bce0c30be1ff366ccce63d
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2017-12-02 23:20:07 +0100

    Added a basic typelist template

---
+++ src/noname_tools/typelist_tools.h
@@ -0,0 +1,104 @@
+//	MIT License
+//
+//	Copyright (c) 2017 Fabian Löschner
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+#include "utility_tools.h"
+
+#if defined(_MSC_VER)
+#define NONAME_EBO __declspec(empty_bases)
+#else
+#define NONAME_EBO
+#endif
+
+// TODO: Allow non-unique typelists (we have to be careful with inheritance)
+
+namespace noname
+{
+	namespace tools
+	{
+		template <typename T>
+		struct type_alias
+		{
+			using type = T;
+		};
+
+		template <typename... Ts>
+		struct NONAME_EBO typelist : public type_alias<Ts>... {};
+
+		template <typename T>
+		struct typelist_size;
+
+		template <typename... Ts>
+		struct typelist_size<typelist<Ts...>> : std::integral_constant<std::size_t, sizeof...(Ts)>
+		{};
+
+		template <typename T>
+		constexpr std::size_t typelist_size_v = typelist_size<T>::value;
+
+		template <typename... Ts>
+		constexpr std::size_t typelist_size_v<typelist<Ts...>> = sizeof...(Ts);
+
+		template <std::size_t I, typename T>
+		struct nth_typelist_element;
+
+		template <std::size_t I, typename... Ts>
+		struct nth_typelist_element<I, typelist<Ts...>>
+		{
+			using type = nth_element_t<I, Ts...>;
+		};
+
+		template <std::size_t I, typename T>
+		using nth_typelist_element_t = typename nth_typelist_element<I, T>::type;
+
+		namespace _detail
+		{
+			template <typename Typelist, typename F, std::size_t ...Indices>
+			F typelist_for_each(Typelist&& tl, F f, std::index_sequence<Indices...>)
+			{
+				using swallow = int[];
+				(void)swallow {
+					1, (f(static_cast<type_alias<nth_typelist_element_t<Indices, typename std::decay<Typelist>::type>>>(tl)), void(), int{})...
+				};
+				return f;
+			}
+		}
+
+		//! Calls a function for each element of a typelist in order and returns the function, the function is called with a type_alias<T> for each T from the list
+		template <typename Typelist, typename F>
+		F typelist_for_each(F f)
+		{
+			static constexpr Typelist tl;
+			static constexpr std::size_t N(typelist_size_v<std::remove_reference_t<Typelist>>);
+			return _detail::typelist_for_each(
+				tl,
+				std::forward<F>(f),
+				std::make_index_sequence<N>{}
+			);
+		}
+	}
+}
+
+#undef NONAME_EBO
---
+++ test_tool/test_typelist.cpp
@@ -0,0 +1,68 @@
+//	MIT License
+//
+//	Copyright (c) 2017 Fabian Löschner
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+
+#include <noname_tools/tools>
+
+#include "catch.hpp"
+
+using namespace noname;
+
+#include <iostream>
+
+TEST_CASE("Testing typelist")
+{
+	using idc_typelist = tools::typelist<int, double, char, float>;
+
+	SECTION("Making static tests")
+	{
+		static_assert(tools::typelist_size<idc_typelist>::value == 4, "typelist_size should have the correct value");
+		static_assert(tools::typelist_size_v<idc_typelist> == 4, "typelist_size should have the correct value");
+
+		static_assert(std::is_same<tools::nth_typelist_element<0, idc_typelist>::type, int>::value, "nth_typelist_element should have the correct type");
+		static_assert(std::is_same<tools::nth_typelist_element<1, idc_typelist>::type, double>::value, "nth_typelist_element should have the correct type");
+		static_assert(std::is_same<tools::nth_typelist_element<2, idc_typelist>::type, char>::value, "nth_typelist_element should have the correct type");
+
+		static_assert(std::is_same<tools::nth_typelist_element_t<0, idc_typelist>, int>::value, "nth_typelist_element should have the correct type");
+		static_assert(std::is_same<tools::nth_typelist_element_t<1, idc_typelist>, double>::value, "nth_typelist_element should have the correct type");
+		static_assert(std::is_same<tools::nth_typelist_element_t<2, idc_typelist>, char>::value, "nth_typelist_element should have the correct type");
+
+		static_assert(sizeof(idc_typelist) == 1, "Typelist size has to of size one (EBO)");
+	}
+
+	SECTION("Testing regular typelist_for_each")
+	{
+		double result = 0;
+		tools::typelist_for_each<idc_typelist>([&result](auto ta) {
+			using type = typename decltype(ta)::type;
+			result += (type(256) / type(3)) + type(10);
+		});
+
+		double comparison = 0;
+		comparison += (int(256) / int(3)) + int(10);
+		comparison += (double(256) / double(3)) + double(10);
+		comparison += (char(256) / char(3)) + char(10);
+		comparison += (float(256) / float(3)) + float(10);
+
+		REQUIRE(result == comparison);
+	}
+
+}
