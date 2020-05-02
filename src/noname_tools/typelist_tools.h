72ad0ae15ee9027e0745c00f4fe3738b2484bb27
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2020-05-03 00:10:26 +0200

    Update copyright

--- src/noname_tools/typelist_tools.h
+++ src/noname_tools/typelist_tools.h
@@ -2,7 +2,7 @@
 
 //	MIT License
 //
-//	Copyright (c) 2017 Fabian Löschner
+//	Copyright (c) 2020 Fabian Löschner
 //
 //	Permission is hereby granted, free of charge, to any person obtaining a copy
 //	of this software and associated documentation files (the "Software"), to deal
